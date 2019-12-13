358f9ae7147b033a4640413a8e3b16a4ac736327
Qwanead <qwanead@protonmail.com>
2019-12-13 17:54:53 +0500

    добавляет стилизацию и адаптив для подвала

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,28 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var siteMapTitle = document.querySelector('.footer__site-map-wrapper .footer__button-wrapper');
+var siteMapButton = document.querySelector('.footer__site-map button');
+var siteMapList = document.querySelector('.footer__site-map-list');
 
-pageHeader.classList.remove('page-header--nojs');
+if (siteMapTitle && siteMapButton && siteMapList) {
+  siteMapButton.classList.toggle('footer__button--close');
+  siteMapList.classList.toggle('footer__site-map-list--hide');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  siteMapTitle.addEventListener('click', function () {
+    siteMapButton.classList.toggle('footer__button--close');
+    siteMapList.classList.toggle('footer__site-map-list--hide');
+  });
+}
+
+var officeTitle = document.querySelector('.footer__office .footer__button-wrapper');
+var officeButton = document.querySelector('.footer__office button');
+var officeList = document.querySelector('.footer__office-list');
+
+if (officeTitle && officeButton && officeList) {
+  officeButton.classList.toggle('footer__button--close');
+  officeList.classList.toggle('footer__office-list--hide');
+
+  officeTitle.addEventListener('click', function () {
+    officeButton.classList.toggle('footer__button--close');
+    officeList.classList.toggle('footer__office-list--hide');
+  });
+}
