4ad4e7b443d0007d816aeb40629acd8d9aab3c32
Qwanead <qwanead@protonmail.com>
2019-12-20 15:36:58 +0500

    переписал плавный скролл без forEach

--- source/js/main.js
+++ source/js/main.js
@@ -70,38 +70,35 @@ if (siteMapTitle && siteMapButton && siteMapList &&
 // собираем все якоря; устанавливаем время анимации и количество кадров
 var feedbackLink = document.querySelector('a[href="#feedback-id"]');
 var advantagesLink = document.querySelector('a[href="#advantages-id"]');
-var anchors = [feedbackLink, advantagesLink];
 var animationTime = 300;
 var framesCount = 20;
 
-anchors.forEach(function (item) {
-  // каждому якорю присваиваем обработчик события
-  item.addEventListener('click', function (e) {
-    // убираем стандартное поведение
-    e.preventDefault();
+var onAnchorClick = function (e) {
+  e.preventDefault();
+  // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+  var coordY = document.querySelector(e.target.closest('a').getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
 
-    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-    var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+  // запускаем интервал, в котором
+  var scroller = setInterval(function () {
+    // считаем на сколько скроллить за 1 такт
+    var scrollBy = coordY / framesCount;
 
-    // запускаем интервал, в котором
-    var scroller = setInterval(function () {
-      // считаем на сколько скроллить за 1 такт
-      var scrollBy = coordY / framesCount;
+    // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+    // и дно страницы не достигнуто
+    if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+      // то скроллим на к-во пикселей, которое соответствует одному такту
+      window.scrollBy(0, scrollBy);
+    } else {
+      // иначе добираемся до элемента и выходим из интервала
+      window.scrollTo(0, coordY);
+      clearInterval(scroller);
+    }
+  // время интервала равняется частному от времени анимации и к-ва кадров
+  }, animationTime / framesCount);
+};
 
-      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-      // и дно страницы не достигнуто
-      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        // то скроллим на к-во пикселей, которое соответствует одному такту
-        window.scrollBy(0, scrollBy);
-      } else {
-        // иначе добираемся до элемента и выходим из интервала
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-    // время интервала равняется частному от времени анимации и к-ва кадров
-    }, animationTime / framesCount);
-  });
-});
+feedbackLink.addEventListener('click', onAnchorClick);
+advantagesLink.addEventListener('click', onAnchorClick);
 
 // Попап
 
